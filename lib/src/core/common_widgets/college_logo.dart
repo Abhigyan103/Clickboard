85281fd2fcb546ab1d337f57fed25aa98a85df71
Avirup Nandi <106444983+avirupnandi1@users.noreply.github.com>
2024-01-10 21:03:56 +0000

    Fix #16: now onTapping logo button you can redirect to college website

--- lib/src/core/common_widgets/college_logo.dart
+++ lib/src/core/common_widgets/college_logo.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 
 import '../constants/image_strings.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CollegeLogo extends StatelessWidget {
   const CollegeLogo({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+Widget build(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: InkWell(
+      onTap: () async {
+        const url = 'https://jgec.ac.in/'; // Replace with the actual website URL
+        if (await canLaunch(url)) {
+          await launch(url);
+        } else {
+          // Handle the case where the URL cannot be launched
+          throw 'Could not launch $url';
+        }
+      },
       child: SizedBox.square(
-          dimension: 50,
-          child: Image.asset(collegeLogo,
-              color: Theme.of(context).brightness == Brightness.light
-                  ? Colors.black
-                  : Colors.white)),
-    );
-  }
+        dimension: 50,
+        child: Image.asset(collegeLogo,
+            color: Theme.of(context).brightness == Brightness.light
+                ? Colors.black
+                : Colors.white),
+      ),
+    ),
+  );
 }
