faec82aef3fc227cdfff4c608906f68f08c168bd
Abhigyan Prakash Singh <abhigyansingh56@gmail.com>
2024-01-17 22:24:02 +0530

    Updated Student Model

--- lib/src/features/authentication/screens/login/login_page.dart
+++ lib/src/features/authentication/screens/login/login_page.dart
@@ -3,7 +3,6 @@ import 'package:clickboard/src/features/authentication/controllers/auth_controll
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:go_router/go_router.dart';
 import 'package:lottie/lottie.dart';
 
 import '../../../../core/constants/image_strings.dart';
