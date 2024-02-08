70910e4b4021253aa1fdf4cc5abe9374fcdd924a
prerna12s <142971343+prerna12s@users.noreply.github.com>
2024-02-08 14:32:16 +0530

    Added animated icons in bottom navigation bar (#84)

--- lib/src/core/common_widgets/my_bottom_navigation_bar.dart
+++ lib/src/core/common_widgets/my_bottom_navigation_bar.dart
@@ -1,21 +1,28 @@
+import 'package:clickboard/src/core/common_widgets/navbar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-
+import 'package:lottie/lottie.dart';
 import '../../providers/utils_providers.dart';
+import '../../models/Nav_Item.dart';
+
+
 
 class MyBottomNavigationBar extends ConsumerWidget {
-  const MyBottomNavigationBar({super.key});
-  List<GButton> buildBottomNavBarItems() {
-    return const [
-      GButton(icon: Icons.home_filled, text: 'Dashboard'),
-      GButton(icon: Icons.edit_document, text: 'Documents'),
-      GButton(icon: Icons.document_scanner, text: 'Result'),
-      GButton(icon: Icons.perm_identity, text: 'Account'),
+  const MyBottomNavigationBar({Key? key}) : super(key: key);
+
+  List<NavItem> buildBottomNavBarItems() {
+    return [
+      NavItem(animationAsset: 'assets/lottie/house.json', text: 'Dashboard'),
+      NavItem(animationAsset: 'assets/lottie/documents.json', text: 'Documents'),
+      NavItem(animationAsset: 'assets/lottie/files.json', text: 'Result'),
+      NavItem(animationAsset: 'assets/lottie/human.json', text: 'Account'),
     ];
   }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final selectedIndex = ref.watch(navigationIndexProvider);
+
     return Container(
       decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.background,
@@ -23,22 +30,29 @@ class MyBottomNavigationBar extends ConsumerWidget {
               top: BorderSide(
                   color: Color.fromARGB(255, 82, 82, 82), width: 0.1))),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GNav(
-          textStyle: Theme.of(context).textTheme.titleSmall,
-          gap: 8,
-          color: Theme.of(context).colorScheme.secondary,
-          activeColor: Theme.of(context).colorScheme.primary,
-          selectedIndex: ref.watch(navigationIndexProvider),
-          tabActiveBorder:
-              Border.all(color: Theme.of(context).colorScheme.outlineVariant),
-          padding: const EdgeInsets.all(15),
-          tabs: buildBottomNavBarItems(),
-          onTabChange: (value) {
-            ref.read(navigationIndexProvider.notifier).update(value);
-          },
-        ),
+        padding: const EdgeInsets.all(5.0),
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: List.generate(
+    buildBottomNavBarItems().length,
+    (index) {
+    final currentIndex = ref.watch(navigationIndexProvider);
+    return Navbaritems(
+      animationWidth:60.0,
+      animationHeight: 40.0,
+      animationAsset: buildBottomNavBarItems()[index].animationAsset,
+       text: buildBottomNavBarItems()[index].text,
+       isSelected: index == currentIndex,
+       onTap: () {
+       ref.read(navigationIndexProvider.notifier).update(index);
+        },
+        activeColor: Theme.of(context).colorScheme.primary,
+        inactiveColor: Theme.of(context).colorScheme.secondary
+      );
+      },
       ),
-    );
-  }
+    ),
+    )
+
+    );}
 }
